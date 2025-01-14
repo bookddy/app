fdbf483093f15482f62962f43c96a7ae901353b7
Andrea Velez <andrea.velez@thoughtworks.com>
2025-01-14 16:05:24 -0500

    Include Register test

--- frontend/src/app/auth/register/register.test.tsx
+++ frontend/src/app/auth/register/register.test.tsx
@@ -1,7 +1,49 @@
 import {render, screen} from '@testing-library/react';
 import Register from './page';
 
-it("renders register page", () => {
-    render(<Register/>);
-    expect(screen.getByText('hello register')).toBeInTheDocument();
+jest.mock('../../../api/authAPI');
+
+const newUserMock = {
+    name: 'Andrea Lopez',
+    username: 'alopez',
+    password:'asdfghj',
+    email:'alopez@gmail.com'
+}
+
+const setupPageLoad = () => {
+    render(<Register />);
+};
+
+describe('<Register />', () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    describe('When register form loads', () => {
+        it('Should have the title Register', () => {
+            setupPageLoad();
+            expect(screen.getByRole('heading')).toHaveTextContent('Register');
+        })
+
+        it('Should display tha name field', () => {
+            setupPageLoad();
+            expect(screen.getByRole('textbox', {name: 'Name'})).toBeInTheDocument();
+        })
+
+        it('Should display tha username field', () => {
+            setupPageLoad();
+            expect(screen.getByRole('textbox', {name: 'Username'})).toBeInTheDocument();
+        })
+
+        it('Should display tha password field', () => {
+            setupPageLoad();
+            expect(screen.getByRole('textbox', {name: 'Password'})).toBeInTheDocument();
+        })
+
+        it('Should display tha email field', () => {
+            setupPageLoad();
+            expect(screen.getByRole('textbox', {name: 'Email'})).toBeInTheDocument();
+        })
+    });
+
 })

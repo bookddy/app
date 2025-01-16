51ea6f4ce037eaa0c4079dbdc9d2633920b93b0c
practicatto <practicatto@gmail.com>
2025-01-16 10:45:19 -0500

    add required field test

--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -1,13 +1,15 @@
+"use client";
 import * as React from "react";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Button from '@mui/material/Button';
-import { FormControl } from "@mui/base/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
 
 export default function Register() {
+  const [name, setName] = React.useState("");
+
   return (
     <Box
       component="form"
@@ -17,42 +19,36 @@ export default function Register() {
     >
       <h1>Register</h1>
       <div>
-          <TextField
-            required
-            id="Name"
-            // data-testid="name"
-            label="Name"
-            defaultValue="Name"
-          />
-        <FormControl defaultValue="" required>
-          <TextField
-            required
-            id="Username"
-            // data-testid="username"
-            label="Username"
-            defaultValue="Username"
-          />
-        </FormControl>
-        <FormControl defaultValue="" required>
-          <TextField
-            required
-            id="Email"
-            // data-testid="email"
-            label="Email"
-            type="email"
-            defaultValue="Email"
-          />
-        </FormControl>
+        <TextField
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          error={name === ""}
+          id="Name"
+          label="Name"
+          defaultValue="Name"
+          helperText={name === "" ? "*Name is required" : ""}
+        />
+        <TextField
+          required
+          id="Username"
+          label="Username"
+          defaultValue="Username"
+        />
+        <TextField
+          required
+          id="Email"
+          label="Email"
+          type="email"
+          defaultValue="Email"
+        />
 
-        <FormControl defaultValue="" required>
-          <TextField
-            id="Password"
-            // data-testid="password"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-          />
-        </FormControl>
+        <TextField
+          id="Password"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
       </div>
 
       <div>
@@ -62,41 +58,9 @@ export default function Register() {
             label="Label"
           />
         </FormGroup>
-        
+
         <Button variant="outlined">Submit</Button>
       </div>
     </Box>
-
   );
 }
-
-// export default function Register() {
-//     return (
-//         <div>
-//             <h1>Register</h1>
-//             <form>
-//                 <label>
-//                     Name
-//                     <input type="text" name="name" />
-//                 </label>
-//                 <label>
-//                     Username
-//                     <input type="text" name="username" />
-//                 </label>
-//                 <label>
-//                     Password
-//                     <input type="password" name="password" />
-//                 </label>
-//                 <label>
-//                     Email
-//                     <input type="email" name="email" />
-//                 </label>
-//                 <label>
-//                     <input type="checkbox" name="terms" />
-//                     I agree to the terms and conditions
-//                 </label>
-//                 <button type="submit">Submit</button>
-//             </form>
-//         </div>
-//     );
-// }
--- frontend/src/app/auth/register/register.test.tsx
+++ frontend/src/app/auth/register/register.test.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Register from './page';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Register from "./page";
+import userEvent from "@testing-library/user-event";
 
 const setupPageLoad = () => {
   return render(<Register />);
 };
 
-describe('<Register />', () => {
+describe("<Register />", () => {
   beforeEach(() => {
     setupPageLoad();
   });
@@ -15,38 +16,76 @@ describe('<Register />', () => {
     jest.resetAllMocks();
   });
 
-  describe('When register form loads', () => {
-    it('Should have the title Register', () => {
-        expect(screen.getByRole('heading')).toHaveTextContent('Register');
+  const mockForm = {
+    name: "John Doe",
+    username: "johndoe",
+    password: "password",
+    email: "john.doe@gmail.com",
+  };
+
+  const fillAndSubmitForm = async (mockForm, submit = true) => {
+    const nameField = screen.getByRole("textbox", { name: /Name/ });
+    const usernameField = screen.getByRole("textbox", { name: /Username/i });
+    const passwordField = screen.getByLabelText(/password/i);
+    const emailField = screen.getByRole("textbox", { name: /Email/i });
+
+    await userEvent.type(nameField, mockForm.name || "{tab}");
+    await userEvent.type(usernameField, mockForm.username);
+    await userEvent.type(passwordField, mockForm.password);
+    await userEvent.type(emailField, mockForm.email);
+
+    if (submit) {
+      const submitButton = screen.getByRole("button", { name: "Submit" });
+      userEvent.click(submitButton);
+    }
+  };
+
+  describe("When register form loads", () => {
+    it("Should have the title Register", () => {
+      expect(screen.getByRole("heading")).toHaveTextContent("Register");
     });
 
-    it('Should display the name field', () => {
-        expect(screen.getByRole('textbox', { name: 'Name' })).toBeInTheDocument();
+    it("Should display the name field", () => {
+      expect(screen.getByRole("textbox", { name: "Name" })).toBeInTheDocument();
     });
 
-    it('Should display the username field', () => {
-        expect(screen.getByRole('textbox', { name: 'Username' })).toBeInTheDocument();
+    it("Should display the username field", () => {
+      expect(
+        screen.getByRole("textbox", { name: "Username" })
+      ).toBeInTheDocument();
     });
 
-    it('Should display the password field', () => {
-        expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+    it("Should display the password field", () => {
+      expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
     });
 
-    it('Should display the email field', () => {
-        expect(screen.getByRole('textbox', { name: 'Email' })).toBeInTheDocument();
+    it("Should display the email field", () => {
+      expect(
+        screen.getByRole("textbox", { name: "Email" })
+      ).toBeInTheDocument();
     });
 
-    it.skip('Should display the submit button', () => {
+    it.skip("Should display the submit button", () => {
       expect(screen.getByText(/submit/i)).toBeInTheDocument();
     });
 
-    it.skip('Should display a terms and conditions checkbox', () => {
-      expect(screen.getByRole('checkbox')).toBeInTheDocument();
-      expect(screen.getByLabelText('I agree to the terms and conditions')).toBeInTheDocument();
+    it.skip("Should display a terms and conditions checkbox", () => {
+      expect(screen.getByRole("checkbox")).toBeInTheDocument();
+      expect(
+        screen.getByLabelText("I agree to the terms and conditions")
+      ).toBeInTheDocument();
     });
 
-    it.skip('Should display an error message placeholder for invalid inputs', () => {
+    it.skip("Should display an error message placeholder for invalid inputs", () => {
       expect(screen.queryByText(/error/i)).not.toBeInTheDocument();
+    });
+  });
+
+  describe("When form has invalid fields", () => {
+    it.skip("Should display an error message when empty name field", () => {
+      setupPageLoad();
+      fillAndSubmitForm({ ...mockForm, name: "" }, false);
+      expect(screen.getByText("*Name is required")).toBeInTheDocument();
     });
   });
 });

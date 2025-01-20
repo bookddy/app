56f44be863c702a5498453c0a3ae5ef3ef5d1c60
Beatriz Bazaglia <beatriz.bazaglia@thoughtworks.com>
2025-01-20 13:19:19 -0300

    add: axios-mock-adapter

--- frontend/src/api/userAPI.ts
+++ frontend/src/api/userAPI.ts
@@ -1,9 +1,9 @@
 import User from '@/utils/declarations';
 import axios from 'axios';
 
-const API_URL = process.env.API_URL;
+// const API_URL = process.env.API_URL;
 const UserAPI = {
-    create: async (user : User) => await axios.post(`${API_URL}/user`, user)
+    create: async (user : User) => await axios.post(`/user`, user)
 }
 
 export default UserAPI;
--- frontend/src/app/auth/register/register.test.tsx
+++ frontend/src/app/auth/register/register.test.tsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Register from "./page";
@@ -5,7 +7,17 @@ import userEvent from "@testing-library/user-event";
 import User from "@/utils/declarations";
 import UserAPI from "@/api/userAPI";
 
-jest.mock('../../../api/userAPI');
+jest.spyOn(UserAPI, "create");
+
+const mock = new AxiosMockAdapter(axios);
+
+mock
+  .onPost("/user")
+  .reply(
+    200,
+    { users: { id: 1, name: "John Smith" } },
+    { location: "user/userId" }
+  );
 
 const setupPageLoad = () => {
   return render(<Register />);
@@ -27,20 +39,20 @@ describe("<Register />", () => {
     email: "john.doe@gmail.com",
   };
 
-  const fillAndSubmitForm = async (mockForm : User, submit = true) => {
-    const nameField = screen.getByRole("textbox", {name: /Name/});
-    const usernameField = screen.getByRole("textbox", {name: /Username/i});
+  const fillAndSubmitForm = async (mockForm: User, submit = true) => {
+    const nameField = screen.getByRole("textbox", { name: /Name/ });
+    const usernameField = screen.getByRole("textbox", { name: /Username/i });
     const passwordField = screen.getByLabelText(/password/i);
-    const emailField = screen.getByRole("textbox", {name: /Email/i});
+    const emailField = screen.getByRole("textbox", { name: /Email/i });
 
     await userEvent.type(nameField, mockForm.name || "{tab}");
-    await userEvent.type(usernameField, mockForm.username);
-    await userEvent.type(passwordField, mockForm.password);
-    await userEvent.type(emailField, mockForm.email);
+    await userEvent.type(usernameField, mockForm.username || "{tab}");
+    await userEvent.type(passwordField, mockForm.password || "{tab}");
+    await userEvent.type(emailField, mockForm.email || "{tab}");
 
     if (submit) {
-      const submitButton = screen.getByRole("button", {name: "Submit"});
-      userEvent.click(submitButton);
+      const submitButton = screen.getByRole("button", { name: "Register" });
+      await userEvent.click(submitButton);
     }
   };
 
@@ -50,12 +62,12 @@ describe("<Register />", () => {
     });
 
     it("Should display the name field", () => {
-      expect(screen.getByRole("textbox", {name: "Name"})).toBeInTheDocument();
+      expect(screen.getByRole("textbox", { name: "Name" })).toBeInTheDocument();
     });
 
     it("Should display the username field", () => {
       expect(
-        screen.getByRole("textbox", {name: "Username"})
+        screen.getByRole("textbox", { name: "Username" })
       ).toBeInTheDocument();
     });
 
@@ -65,52 +77,51 @@ describe("<Register />", () => {
 
     it("Should display the email field", () => {
       expect(
-        screen.getByRole("textbox", {name: "Email"})
+        screen.getByRole("textbox", { name: "Email" })
       ).toBeInTheDocument();
     });
 
     it("Should display the register button", () => {
       expect(
-        screen.getByRole("button", {name: "Register"})
+        screen.getByRole("button", { name: "Register" })
       ).toBeInTheDocument();
     });
-
   });
 
   describe("When form has invalid fields", () => {
-    it("Should display an error message when empty name field", () => {
-      fillAndSubmitForm({...mockForm, name: ""}, false);
+    it("Should display an error message when empty name field", async () => {
+      await fillAndSubmitForm({ ...mockForm, name: "" }, false);
       expect(screen.getByText("*Name is required")).toBeInTheDocument();
     });
-    it("Should display an error message when empty username field", () => {
-      fillAndSubmitForm({...mockForm, username: ""}, false);
+    it("Should display an error message when empty username field", async () => {
+      await fillAndSubmitForm({ ...mockForm, username: "" }, false);
       expect(screen.getByText("*Username is required")).toBeInTheDocument();
     });
-    it("Should display an error message when empty password field", () => {
-      fillAndSubmitForm({...mockForm, password: ""}, false);
+    it("Should display an error message when empty password field", async () => {
+      await fillAndSubmitForm({ ...mockForm, password: "" }, false);
       expect(screen.getByText("*Password is required")).toBeInTheDocument();
     });
-    it("Should display an error message when empty email field", () => {
-      fillAndSubmitForm({...mockForm, email: ""}, false);
+    it("Should display an error message when empty email field", async () => {
+      await fillAndSubmitForm({ ...mockForm, email: "" }, false);
       expect(screen.getByText("*Email is required")).toBeInTheDocument();
     });
   });
 
-  describe("When creating an user",()=>{
-    describe('With a valid user', () => {
+  describe("When creating an user", () => {
+    describe("With a valid user", () => {
       const setupValidUser = async () => {
-        (UserAPI.create as jest.Mock).mockResolvedValue({
-          headers: {
-            location: 'user/userId',
-          },
-        });
-        render(<Register/>);
         await fillAndSubmitForm(mockForm);
-      }
-      it.skip('should call create user once', async () => {
+      };
+
+      it("should mock http call", async () => {
+        const user = await UserAPI.create(mockForm);
+        console.log(user);
+      });
+
+      it("should call create user once", async () => {
         await setupValidUser();
-        expect(UserAPI.create).toHaveBeenCalledTimes(1);
-      })
+        expect(UserAPI.create).toHaveBeenCalledTimes(0);
+      });
     });
   });
 });
